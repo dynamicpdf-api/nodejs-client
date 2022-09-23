932f1ab5e7fdcad598544158354a90f506c7920f
Sheetal S <sheetal@dynamicpdf.com>
2022-09-24 01:44:58 +0530

    Committing initial code implementation.

---
+++ test/testHtmlToPDF.js
@@ -0,0 +1,98 @@
+import fs from 'fs';
+import assert from 'assert';
+import { TestParams } from './init.js';
+import { Html } from '../lib/Html.js';
+import { HtmlInput } from '../lib/HtmlInput.js';
+import { PageSize } from '../lib/PageSize.js';
+import { pageSizeName } from './imports.js';
+import { orientation } from '../lib/orientation.js';
+
+function getEndpoint(testParams) {
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+
+    var endpoint = new Html();
+    endpoint.loggingEnabled = testParams.Logging;
+    if (testParams.BaseUrl.length > 0) {
+        endpoint.baseUrl = testParams.BaseUrl;
+    }
+    if (testParams.ApiKey.length > 0) {
+        endpoint.apiKey = testParams.ApiKey;
+    }
+    endpoint.author = "sheetal";
+    endpoint.title = "pdf merger";
+    return endpoint;
+}
+
+describe('HTML to PDF', function () {
+    this.timeout(0);
+    var testParams = new TestParams();
+    it('Using string', async function () {
+        var htmlEndpoint = getEndpoint(testParams);
+        var input = new HtmlInput("<html><body>hello</body></html>");
+        var pageSize= new PageSize();
+        pageSize.Name=pageSizeName.A5;
+        htmlEndpoint.PageSize=pageSize;
+        htmlEndpoint.Inputs = input;
+        var res = await htmlEndpoint.process();
+        
+            if (res.isSuccessful) {
+                var outStream = fs.createWriteStream("./output/HtmlString.pdf");
+                outStream.write(res.content);
+                outStream.close();
+            }
+        
+        assert.strictEqual(res.isSuccessful, true);
+
+    });
+    it('Using string and basepath', async function () {
+        var htmlEndpoint = getEndpoint(testParams);
+        var input = new HtmlInput("<html><body>hello<img src='cat.png'></img></body></html>","./imageResources/");
+        var pageSize= new PageSize(pageSizeName.DoublePostcard);
+        htmlEndpoint.PageSize=pageSize;
+        htmlEndpoint.Inputs = input;
+        var res = await htmlEndpoint.process();
+        
+            if (res.isSuccessful) {
+                var outStream = fs.createWriteStream("./output/HtmlStringAndBasepath.pdf");
+                outStream.write(res.content);
+                outStream.close();
+            }
+        
+        assert.strictEqual(res.isSuccessful, true);
+
+    });
+    it('Using resource', async function () {
+        var htmlEndpoint = getEndpoint(testParams);
+        var input = new HtmlInput('./Resources/htmlSample.html');
+        var pageSize= new PageSize(600,500);
+        htmlEndpoint.PageSize=pageSize;
+        htmlEndpoint.Inputs = input;
+        var res = await htmlEndpoint.process();
+        
+            if (res.isSuccessful) {
+                var outStream = fs.createWriteStream("./output/HtmlResource.pdf");
+                outStream.write(res.content);
+                outStream.close();
+            }
+        
+        assert.strictEqual(res.isSuccessful, true);
+
+    });
+    it('Using string and Margin', async function () {
+        var htmlEndpoint = getEndpoint(testParams);
+        var input = new HtmlInput("<html><body>hello</body></html>");
+        htmlEndpoint.Orientation=orientation.landscape;
+        htmlEndpoint.TopMargin=300;
+        htmlEndpoint.Inputs = input;
+        var res = await htmlEndpoint.process();
+        
+            if (res.isSuccessful) {
+                var outStream = fs.createWriteStream("./output/HtmlStringWithMargin.pdf");
+                outStream.write(res.content);
+                outStream.close();
+            }
+        
+        assert.strictEqual(res.isSuccessful, true);
+
+    });
+});
