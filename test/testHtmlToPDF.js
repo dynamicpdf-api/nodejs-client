5fdba8554db442a1094319d8b22fe888935578fe
SheetalS <sheetal@dynamicpdf.com>
2023-01-06 18:23:08 +0530

    Changes to resolve warnings and support logging in HtmlToPDF.

--- test/testHtmlToPDF.js
+++ test/testHtmlToPDF.js
@@ -8,8 +8,9 @@ import { HtmlResource } from '../lib/HtmlResource.js';
 import { Pdf } from './imports.js';
 
 function getEndpoint(testParams) {
-    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
-
+    if (testParams.AuthTLS == false) {
+        process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+    }
     var endpoint = new Pdf();
     endpoint.loggingEnabled = testParams.Logging;
     if (testParams.BaseUrl.length > 0) {
@@ -29,86 +30,91 @@ describe('HTML to PDF', function () {
     it('Using string', async function () {
         var resource = new HtmlResource("<html><body>hello</body></html>");
         var pdfEndpoint = getEndpoint(testParams);
-        var input = new HtmlInput(resource,PageSize.DoublePostcard);
+        var input = new HtmlInput(resource, PageSize.DoublePostcard);
         pdfEndpoint.inputs.push(input);
         var res = await pdfEndpoint.process();
-
-        if (res.isSuccessful) {
-            var outStream = fs.createWriteStream("./output/HtmlString.pdf");
-            outStream.write(res.content);
-            outStream.close();
+        if (testParams.Logging) {
+            console.log("Result: " + res.isSuccessful);
+            if (res.isSuccessful) {
+                var outStream = fs.createWriteStream("./output/HtmlString.pdf");
+                outStream.write(res.content);
+                outStream.close();
+            }
         }
-
         assert.strictEqual(res.isSuccessful, true);
 
     });
     it('Using string and basepath', async function () {
         var resource = new HtmlResource("<html><body><h1> HELLO</h1> <img src='googlelogo_color_272x92dp.png' /></body></html>");
         var pdfEndpoint = getEndpoint(testParams);
-        var input = new HtmlInput(resource,PageSize.DoublePostcard,Orientation.landscape);
-        input.BasePath= "https://www.google.com/images/branding/googlelogo/1x/";
+        var input = new HtmlInput(resource, PageSize.DoublePostcard, Orientation.landscape);
+        input.BasePath = "https://www.google.com/images/branding/googlelogo/1x/";
         pdfEndpoint.inputs.push(input);
         var res = await pdfEndpoint.process();
-
-        if (res.isSuccessful) {
-            var outStream = fs.createWriteStream("./output/HtmlStringAndBasepath.pdf");
-            outStream.write(res.content);
-            outStream.close();
+        if (testParams.Logging) {
+            console.log("Result: " + res.isSuccessful);
+            if (res.isSuccessful) {
+                var outStream = fs.createWriteStream("./output/HtmlStringAndBasepath.pdf");
+                outStream.write(res.content);
+                outStream.close();
+            }
         }
-
         assert.strictEqual(res.isSuccessful, true);
 
     });
     it('Using string and Margin', async function () {
         var resource = new HtmlResource("<html><body>hello</body></html>");
         var pdfEndpoint = getEndpoint(testParams);
-        var input = new HtmlInput(resource,PageSize.DoublePostcard);
+        var input = new HtmlInput(resource, PageSize.DoublePostcard);
         input.Orientation = Orientation.landscape;
         input.TopMargin = 300;
         pdfEndpoint.inputs.push(input);
         var res = await pdfEndpoint.process();
-
-        if (res.isSuccessful) {
-            var outStream = fs.createWriteStream("./output/HtmlStringWithMargin.pdf");
-            outStream.write(res.content);
-            outStream.close();
+        if (testParams.Logging) {
+            console.log("Result: " + res.isSuccessful);
+            if (res.isSuccessful) {
+                var outStream = fs.createWriteStream("./output/HtmlStringWithMargin.pdf");
+                outStream.write(res.content);
+                outStream.close();
+            }
         }
-
         assert.strictEqual(res.isSuccessful, true);
 
     });
     it('Using Resource and Margin', async function () {
         var resource = new HtmlResource("./Resources/htmlSample.html");
         var pdfEndpoint = getEndpoint(testParams);
-        var input = new HtmlInput(resource,"",PageSize.DoublePostcard);
+        var input = new HtmlInput(resource, "", PageSize.DoublePostcard);
         input.PageOrientation = Orientation.landscape;
         input.TopMargin = 300;
         pdfEndpoint.inputs.push(input);
         var res = await pdfEndpoint.process();
-
-        if (res.isSuccessful) {
-            var outStream = fs.createWriteStream("./output/HtmlResourceWithMargin.pdf");
-            outStream.write(res.content);
-            outStream.close();
+        if (testParams.Logging) {
+            console.log("Result: " + res.isSuccessful);
+            if (res.isSuccessful) {
+                var outStream = fs.createWriteStream("./output/HtmlResourceWithMargin.pdf");
+                outStream.write(res.content);
+                outStream.close();
+            }
         }
-
         assert.strictEqual(res.isSuccessful, true);
 
     });
     it('Using Resource and PageSize', async function () {
         var resource = new HtmlResource("./Resources/htmlSample.html");
         var pdfEndpoint = getEndpoint(testParams);
-        var input = new HtmlInput(resource,"" ,PageSize.DoublePostcard);
+        var input = new HtmlInput(resource, "", PageSize.DoublePostcard);
         //input.PageSize=PageSize.A3;
         pdfEndpoint.inputs.push(input);
         var res = await pdfEndpoint.process();
-
-        if (res.isSuccessful) {
-            var outStream = fs.createWriteStream("./output/HtmlResourceWithPageSize.pdf");
-            outStream.write(res.content);
-            outStream.close();
+        if (testParams.Logging) {
+            console.log("Result: " + res.isSuccessful);
+            if (res.isSuccessful) {
+                var outStream = fs.createWriteStream("./output/HtmlResourceWithPageSize.pdf");
+                outStream.write(res.content);
+                outStream.close();
+            }
         }
-
         assert.strictEqual(res.isSuccessful, true);
 
     });
